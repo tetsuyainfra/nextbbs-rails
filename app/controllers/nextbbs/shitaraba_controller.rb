dffba869e956a58f7b316b05cb10d33790e8f077
tetsuyainfra <tetsuyainfra@gmail.com>
2019-11-27 15:49:37 +0900

    update

--- app/controllers/nextbbs/shitaraba_controller.rb
+++ app/controllers/nextbbs/shitaraba_controller.rb
@@ -2,10 +2,29 @@ require_dependency "nextbbs/application_controller"
 
 module Nextbbs
   class ShitarabaController < ApplicationController
+
+    # /
     def index
     end
 
-    def dat
+    # /subject.txt
+    def subject
+      @topics = Topic.all
+      render "nextbbs/shitaraba/subject.txt.erb", content_type: 'text/plain; charset=Shift_JIS'
     end
+
+    def dat
+      @topic = Topic.find(params[:id])
+      @comments = @topic.comments
+      render "nextbbs/shitaraba/dat.txt.erb", content_type: 'text/plain; charset=Shift_JIS'
+    end
+
+    private
+    def change_charset_to_sjis
+      # headers["Content-Type"] = 'text/plain; charset=Shift_JIS'
+      # response.body           = response.body.encode(Encoding::SHIFT_JIS)
+      render "nextbbs/shitaraba/subject.txt.erb", content_type: 'text/plain; charset=Shift_JIS'
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Nextbbs::Engine.routes.draw do
-  get 'shitaraba/index'
-  get 'shitaraba/dat'
   resources :topics do
     resources :comments
   end
+
+  get 'shitaraba/', to: 'shitaraba#index'
+  get 'shitaraba/index'
+  get 'shitaraba/subject.txt', to: 'shitaraba#subject'
+  get 'shitaraba/dat/:id', to: 'shitaraba#dat', default: :dat
+
 end
